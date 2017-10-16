a7feff512087b7ceea70df4bfef9769a8b470db1
Eason2001 <zhicheng2001@gmail.com>
2017-10-16 01:51:21 -0400

    Creating repository files ...

---
+++ src/main/java/uow/csse/bptzz/dao/bptzz_CourseRegRepo.java
@@ -0,0 +1,7 @@
+package uow.csse.bptzz.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface bptzz_CourseRegRepo extends CrudRepository<bptzz_CourseReg, String> {
+    //three primary keys
+}
---
+++ src/main/java/uow/csse/bptzz/dao/bptzz_InstructorDeptRepo.java
@@ -0,0 +1,7 @@
+package uow.csse.bptzz.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface bptzz_InstructorDeptRepo extends CrudRepository<bptzz_InstructorDept, String> {
+    //tow primary keys
+}
