49b953dc028be7b8c1b5b5e9faae509fae083464
Eason2001 <zhicheng2001@gmail.com>
2017-10-16 01:55:52 -0400

    Creating DAO repository ...

---
+++ src/main/java/uow/csse/bptzz/dao/bptzz_Classroom.java
@@ -0,0 +1,59 @@
+package uow.csse.bptzz.dao;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Create the Entity
+ *
+ * @author 	Eason Pan
+ * @date	2017-10-16
+ * @since	1.0
+ *
+ * Database: pro611db
+ * Username: bfa57f3b0eddae
+ * Password: c759e708
+ * Server: ca-cdbr-azure-central-a.cloudapp.net
+ */
+
+@Entity // This tells Hibernate to make a table out of this class
+public class bptzz_Classroom {
+    @Id
+    private String ClassroomID;
+    private String Location;
+    private int    MaxCapacity;
+    private int    SeatsLeft;
+
+    public String getClassroomID() {
+        return ClassroomID;
+    }
+
+    public void setClassroomID(String classroomID) {
+        ClassroomID = classroomID;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public int getMaxCapacity() {
+        return MaxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        MaxCapacity = maxCapacity;
+    }
+
+    public int getSeatsLeft() {
+        return SeatsLeft;
+    }
+
+    public void setSeatsLeft(int seatsLeft) {
+        SeatsLeft = seatsLeft;
+    }
+}
---
+++ src/main/java/uow/csse/bptzz/dao/bptzz_ClassroomCourseRepo.java
@@ -0,0 +1,7 @@
+package uow.csse.bptzz.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface bptzz_ClassroomCourseRepo extends CrudRepository<bptzz_ClassroomCourse, String> {
+    //three primary keys, need to research and update later
+}
